21e57514c55a76c7b5c25342ab23a33a071fb2b0
231250009-Zhou Xu <231250009@smail.nju.edu.cn>
2025-03-20 19:40:38 +0800

    test

--- backend/src/main/java/com/example/tomatomall/configure/MyWebMvcConfig.java
+++ backend/src/main/java/com/example/tomatomall/configure/MyWebMvcConfig.java
@@ -1,2 +1,26 @@
-package com.example.tomatomall.configure;public class MyWebMvcConfig {
+package com.example.tomatomall.configure;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class MyWebMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/api/users/register")
+                .excludePathPatterns("/api/users/login")
+                .excludePathPatterns("/api/stores/{storeId}")
+                .excludePathPatterns("/api/stores/store_create")
+                .excludePathPatterns("/api/images/upload")
+                .order(1);
+    }
+
 }
+
--- backend/src/main/java/com/example/tomatomall/util/TokenUtil.java
+++ backend/src/main/java/com/example/tomatomall/util/TokenUtil.java
@@ -1,2 +1,17 @@
-package com.example.tomatomall.util;public class TokenUtil {
+package com.example.tomatomall.util;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.example.tomatomall.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+public class TokenUtil {
+    private static final long EXPIRE_TIME = 24 * 60 * 60 * 1000;
+
+    @Autowired
+    UserRepository userRepository;
+
 }
