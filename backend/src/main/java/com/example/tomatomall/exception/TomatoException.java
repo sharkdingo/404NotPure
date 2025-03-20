8451c25cd55f60fe7d21d5a140e95a57350eb07a
231250009-Zhou Xu <231250009@smail.nju.edu.cn>
2025-03-20 18:49:30 +0800

    完成了yml，拦截器，exception

---
+++ backend/src/main/java/com/example/tomatomall/exception/TomatoException.java
@@ -0,0 +1,34 @@
+package com.example.tomatomall.exception;
+/*
+全局异常
+*/
+public class TomatoException extends RuntimeException{
+
+    private String code;
+
+    public TomatoException(String msg,String code) {
+        super(msg);
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public static TomatoException userNameExist(){
+        return new TomatoException("用户名已存在！","409");
+    }
+
+    public static TomatoException notLogin(){
+        return new TomatoException("未登陆！","401");
+    }
+
+    public static TomatoException loginFailure(){
+        return new TomatoException("用户名或密码错误！","401");
+    }
+
+    public static TomatoException uploadFailure(){
+        return new TomatoException("上传失败！","500");
+    }
+
+}
