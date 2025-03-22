1102a9bbe2f060f8c9ed710bd25bacb341277720
231250009-Zhou Xu <231250009@smail.nju.edu.cn>
2025-03-22 14:35:52 +0800

    拓展了电话号码登陆，前端统一包装为用户名就好

--- backend/src/main/java/com/example/tomatomall/repository/UserRepository.java
+++ backend/src/main/java/com/example/tomatomall/repository/UserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Account, Integer> {
     Optional<Account> findByUsername(String username);
+    Optional<Account> findByTelephone(String telephone);
 }
